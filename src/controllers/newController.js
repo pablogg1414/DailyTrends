4bbf5b6688cd5cd4a904432c87577dd71260d744
Pablo Guindo <pablogg1414@gmail.com>
2022-02-07 22:10:59 +0100

    Implementación editar noticia

--- src/controllers/newController.js
+++ src/controllers/newController.js
@@ -16,8 +16,30 @@ controller.list = (req, res) => {
 controller.save = (req, res) => {
     const data = req.body;
     req.getConnection((err, conn) => {
-        conn.query('INSERT INTO news set ?', [data],(err, insertNew) => {
+        conn.query('INSERT INTO news set ?', [data], (err, insertNew) => {
             console.log(insertNew);
+            res.redirect('/');
+        });
+    });
+};
+
+controller.edit = (req, res) => {
+    const numId = req.params.id;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM news WHERE id = ?', [numId], (err, updateNew) => {
+            console.log(updateNew[0]);
+            res.render('edit_new', {
+                data: updateNew[0]
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+    const numId = req.params.id;
+    const updatedNew = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE news set ? WHERE id = ?', [updatedNew, numId], (err, rows) => {
             res.redirect('/');
         });
     });
@@ -26,7 +48,7 @@ controller.save = (req, res) => {
 controller.delete = (req, res) => {
     const numId = req.params.id;
     req.getConnection((err, conn) => {
-        conn.query('DELETE FROM news where id = ?', [numId],(err, rows) => {
+        conn.query('DELETE FROM news WHERE id = ?', [numId], (err, rows) => {
             res.redirect('/');
         });
     });
--- src/routes/new.js
+++ src/routes/new.js
@@ -7,5 +7,8 @@ router.get('/', newController.list);
 router.post('/add', newController.save);
 router.get('/delete/:id', newController.delete);
 
+router.get('/update/:id', newController.edit);
+router.post('/update/:id', newController.update);
+
 
 module.exports = router;
